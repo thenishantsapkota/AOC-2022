c81183a09eb397dc141f07110ec2ecedddb46a24
Nishant Sapkota <snishant306@gmail.com>
2022-12-08 18:23:39 +0545

    feat: completed Day 1

---
+++ day-1/day_1.py
@@ -0,0 +1,17 @@
+def most_calories():
+    with open("./day-1/input.txt") as f:
+        content = f.read()
+
+    string = content.split("\n\n")
+    sum_array = []
+    for data in string:
+        calories = [int(i) for i in data.split("\n")]
+        sum_array.append(sum(calories))
+    
+    sum_array.sort(reverse=True)
+    print(sum_array[0]) # Elf carrying highest calories
+    print(sum(sum_array[0:3])) # Total calories carried by top 3 elves
+        
+        
+
+most_calories()
